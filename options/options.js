678cd1408f145322aef9993abfc39945e127e644
Crowdbotics <team@crowdbotics.com>
2023-08-21 02:17:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-sunset-42973",
+  url: "https://fancy-sunset-42973.botics.co",
+  api: "https://fancy-sunset-42973.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
